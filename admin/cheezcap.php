9de31d83aba35ee7a790950542f69668a7990a2e
slaFFik <slava.abakumov@gmail.com>
2012-10-15 14:44:29 +0300

    create default options if they don't exist

--- admin/cheezcap.php
+++ admin/cheezcap.php
@@ -16,28 +16,28 @@ require_once( dirname(__FILE__) . '/config.php' );
 
 add_action( 'admin_init', 'custom_community_theme_options_init' );
 function custom_community_theme_options_init(){
-	register_setting( 'custom_community_options', 'custom_community_theme_options', 'custom_community_theme_options_validate' );
+    register_setting( 'custom_community_options', 'custom_community_theme_options', 'custom_community_theme_options_validate' );
 }
 
 add_action( 'admin_init', 'cc_update_old_version' );
 function cc_update_old_version(){
-	if(get_option('cc_version') <= 1.8){
-		$options = wp_load_alloptions();
-		foreach((array) $options as $key => $value) :
-			$key = esc_attr($key);
-	  		if(substr($key, 0, 4)=='cap_') {
-		  		$cap = get_option('custom_community_theme_options');
-		  		$cap[$key] = $value;
-		  		update_option( 'custom_community_theme_options', $cap );
-		  		
-		    	delete_option($key);     
-	  		}
-	  	endforeach;
-	    update_option( 'cc_version', 1.9 );
-	} else if (!get_option('cc_version')){
-		cap_defaults_init();
-		update_option( 'cc_version', 1.9 );
-	}
+    if(get_option('cc_version') <= 1.8){
+        $options = wp_load_alloptions();
+        foreach((array) $options as $key => $value) :
+            $key = esc_attr($key);
+              if(substr($key, 0, 4)=='cap_') {
+                  $cap = get_option('custom_community_theme_options');
+                  $cap[$key] = $value;
+                  update_option( 'custom_community_theme_options', $cap );
+                  
+                delete_option($key);     
+              }
+          endforeach;
+        update_option( 'cc_version', 1.9 );
+    } else if (!get_option('cc_version')){
+        cap_defaults_init();
+        update_option( 'cc_version', 1.9 );
+    }
 }
 
 $cap = new autoconfig();
@@ -48,77 +48,96 @@ if ( ! defined( 'LOADED_CONFIG' ) ) {
 }
 
 function cap_add_admin() {
-	global $themename, $req_cap_to_edit;
+    global $themename, $req_cap_to_edit;
 
-	if ( ! current_user_can ( $req_cap_to_edit ) )
-		return;
+    if ( ! current_user_can ( $req_cap_to_edit ) )
+        return;
 
-	if ( isset( $_GET['page'] ) && $_GET['page'] == 'theme_settings' ) {
-		$options = cap_get_options();
-		$action = isset( $_REQUEST['action'] ) ? $_REQUEST['action'] : '';
-		$method = false;
-		$done = false;
-		$data = new ImportData();
-		switch ( $action ) {
-			case 'Reset':			
-				delete_option('custom_community_theme_options');     
-				cap_defaults_init();				  
-				$method = false;
-				break;
-			case 'Export':
-				$method = 'Export';
-				$done = 'cap_serialize_export';
-				break;
-			case 'Import':
-				$method = 'Import';
-				if(empty($_FILES['file']['tmp_name']))
-					return ;
-				
-				$data = unserialize( implode ('', file ($_FILES['file']['tmp_name'])));
-				break;
-		}
+    if ( isset( $_GET['page'] ) && $_GET['page'] == 'theme_settings' ) {
+        $options = cap_get_options();
+        $action = isset( $_REQUEST['action'] ) ? $_REQUEST['action'] : '';
+        $method = false;
+        $done = false;
+        $data = new ImportData();
+        switch ( $action ) {
+            case 'Reset':            
+                delete_option('custom_community_theme_options');     
+                cap_defaults_init();                  
+                $method = false;
+                break;
+            case 'Export':
+                $method = 'Export';
+                $done = 'cap_serialize_export';
+                break;
+            case 'Import':
+                $method = 'Import';
+                if(empty($_FILES['file']['tmp_name']))
+                    return ;
+                
+                $data = unserialize( implode ('', file ($_FILES['file']['tmp_name'])));
+                break;
+        }
 
-		if ( $method ) {
-			foreach ( $options as $group ) {
-				foreach ( $group->options as $option ) {
-					call_user_func( array( $option, $method ), $data );
-				}
-	    	}
-			if ( $done )
-				call_user_func( $done, $data );
-		}
-	}
+        if ( $method ) {
+            foreach ( $options as $group ) {
+                foreach ( $group->options as $option ) {
+                    call_user_func( array( $option, $method ), $data );
+                }
+            }
+            if ( $done )
+                call_user_func( $done, $data );
+        }
+    }
 
-	$pgName = "$themename Settings";
-	$hook = add_theme_page( $pgName, $pgName, isset( $req_cap_to_edit ) ? $req_cap_to_edit : 'edit_theme_options', 'theme_settings', 'top_level_settings' );
-	add_action( "admin_print_scripts-$hook", 'cap_admin_js_libs' );
-	add_action( "admin_footer-$hook", 'cap_admin_js_footer' );
-	add_action( "admin_print_styles-$hook", 'cap_admin_css' );	
+    $pgName = "$themename Settings";
+    $hook = add_theme_page( $pgName, $pgName, isset( $req_cap_to_edit ) ? $req_cap_to_edit : 'edit_theme_options', 'theme_settings', 'top_level_settings' );
+    add_action( "admin_print_scripts-$hook", 'cap_admin_js_libs' );
+    add_action( "admin_footer-$hook", 'cap_admin_js_footer' );
+    add_action( "admin_print_styles-$hook", 'cap_admin_css' );    
 }
 
+/**
+ * Create default options for the theme and save into the wp_options table
+ */
 function cap_defaults_init(){
-	$cap_options = cap_get_options();
-	
-	$cap_options_default = Array();
-	
+    $cap_options = cap_get_options();
+    
+    $cap_options_default = Array();
+    
 // print_var($cap_options,1);
 
-	foreach( $cap_options as $cap_option ) {
-		$cap_option_arr = (Array) $cap_option;
-		foreach ($cap_option_arr['options'] AS $option){
-			switch(get_class($option)){
-				case 'BooleanOption':
-					$cap_options_default[$option->id] = $option->options[$option->std];
-					break;
-				case 'DropdownOption':
-					$cap_options_default[$option->id] = $option->options[0];
-					break;
-				default:
-					$cap_options_default[$option->id] = $option->std;
-					break;
-			}
-		}
-	}
-	
-	update_option( 'custom_community_theme_options', $cap_options_default );
+    foreach( $cap_options as $cap_option ) {
+        $cap_option_arr = (Array) $cap_option;
+        foreach ($cap_option_arr['options'] AS $option){
+            switch(get_class($option)){
+                case 'BooleanOption':
+                    $cap_options_default[$option->id] = $option->options[$option->std];
+                    break;
+                case 'DropdownOption':
+                    $cap_options_default[$option->id] = $option->options[0];
+                    break;
+                default:
+                    $cap_options_default[$option->id] = $option->std;
+                    break;
+            }
+        }
+    }
+    
+    update_option( 'custom_community_theme_options', $cap_options_default );
+}
+
+/**
+ * Check in admin area that we have options in wp_options table.
+ * If not - create with default values (almost all are set)
+ */
+add_action('admin_print_scripts', 'cc_activation_function');
+function cc_activation_function() {
+    global $pagenow; 
+    //this is hack for inserting default options when theme activated
+    if ( is_admin() && $pagenow == 'themes.php' && $_GET['activated'] == 'true') { 
+            $option = get_option('custom_community_theme_options');
+            if(empty($option)){
+                cap_defaults_init();
+            }
+     }
 }
